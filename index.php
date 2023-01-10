1887aee60f7904b5f7db72397ff0fe8b9e54c3d7
slivcaigor <slivcaigor@gmail.com>
2023-01-10 13:40:45 +0100

    Display Content in a Table

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>PHP Hotel</title>
 
     <?php
@@ -52,16 +54,56 @@
 </head>
 
 <body>
-    <?php
-    foreach ($hotels as $hotel) {
-        echo "Name: " . $hotel['name'] . "<br>";
-        echo "Description: " . $hotel['description'] . "<br>";
-        echo "Parking: " . ($hotel['parking'] ? "Available" : "Not available") . "<br>";
-        echo "Vote: " . $hotel['vote'] . "<br>";
-        echo "Distance to center: " . $hotel['distance_to_center'] . "<br>";
-        echo "<br>";
-    }
-    ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Hotel Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $counter = 1;
+            foreach ($hotels as $hotel) {
+                ?>
+            <tr>
+                <th scope="row">
+                    <?php echo $counter; ?>
+                </th>
+                <td>
+                    <?php echo $hotel['name']; ?>
+                </td>
+                <td>
+                    <?php echo $hotel['description']; ?>
+                </td>
+                <td>
+                    <?php echo $hotel['parking'] ? "Available" : "Not available"; ?>
+                </td>
+                <td>
+                    <?php echo $hotel['vote']; ?>
+                </td>
+                <td>
+                    <?php echo $hotel['distance_to_center']; ?>
+                </td>
+            </tr>
+            <?php
+                $counter++;
+            }
+            ?>
+        </tbody>
+    </table>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
